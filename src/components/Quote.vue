dfe9fffaec234c4a2ab485178a0010a696927e47
arrianne <arrianneoshea@hotmail.co.uk>
2019-06-25 09:55:49 +0100

    setting up the default object which contains the props that will allow me access to the single quotes created in quote.vue, then I will be able to loop through them. Some basic styling also added for the single quotes

---
+++ src/components/Quote.vue
@@ -0,0 +1,33 @@
+<template id="">
+  <div class="row">
+
+  </div>
+
+  </div>
+
+</template>
+
+<script>
+// default object shall have some props named quotes which are passed from outside so that I have access to them
+// with quotes available I can loop through them as soon as I have my quote component
+  export default {
+    props: ['quotes']
+  }
+</script>
+
+<style media="screen">
+  .panel-body {
+    font-family: 'Arizonia', cursive;
+    font-size: 24px;
+    color: #6e6e6e;
+  }
+
+  .quote {
+    cursor: pointer;
+  }
+
+  .quote:hover {
+    background-color: #ffe2e2;
+  }
+
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,16 @@
+<template id="">
+  <div class="row">
+
+  </div>
+
+  </div>
+
+</template>
+
+<script>
+// default object shall have some props named quotes which are passed from outside so that I have access to them
+// with quotes available I can loop through them as soon as I have my quote component
+  export default {
+    props: ['quotes']
+  }
+</script>
