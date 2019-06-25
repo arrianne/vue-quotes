72d91e92cc177ed147782eae8cb008266d65992a
arrianne <arrianneoshea@hotmail.co.uk>
2019-06-25 09:47:25 +0100

    tracking the maximum number of quotes in the data object and setting up the array of quotes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="container">
-    
+
     </div>
 </template>
 
 <script>
+// returns the data object
     export default {
-        
+      data: function() {
+        return {
+          quotes[
+            'Just a quote to see something'
+          ]
+          maxQuotes: 10
+        }
+      }
     }
 </script>
 
