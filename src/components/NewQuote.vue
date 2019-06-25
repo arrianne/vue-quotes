85200b47a06887a8565c2e2e65967fbda443544b
arrianne <arrianneoshea@hotmail.co.uk>
2019-06-25 11:09:49 +0100

    create the add new form where the user updates the new quote method when filling out form. Button also created with a method attached on click

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,8 +1,17 @@
 <template id="">
   <div class="row">
-    <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
-      <label for="">Quote</label>
-    </div>
+    <form class="" action="index.html" method="post">
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label for="">Quote</label>
+        <!-- using v-model to bind whatever the user types in the text area to the quote object below  -->
+        <textarea name="name" rows="3" cols="80" v-model="quote" class="form-control"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <!-- prevent default behaviour. I don't want to submit the form or send the request to the server which would refresh the page. -->
+        <!-- only execute the create new method -->
+        <button type="button" name="button" @click="createNew.prevent" class="btn btn-primary">Add Quote</button>
+      </div>
+    </form>
   </div>
 
   </div>
@@ -10,6 +19,17 @@
 </template>
 
 <script>
+export default {
+  data: function() {
+    return {
+      quote: ''
+    }
+  },
+  methods: {
+    createNew() {
 
+    }
+  }
+}
 
 </script>
